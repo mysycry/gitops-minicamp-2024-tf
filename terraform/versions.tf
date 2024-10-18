be8ccf90c5a8dd4db0f33959b766f281fc4f5028
Birch Tree <98628109+mysycry@users.noreply.github.com>
2024-10-18 02:12:12 +0000

    added S3 and DynamoDB backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,9 @@
-terraform {}
+terraform {
+  required_version = "~> 1.9.5"
+  backend "s3" {
+    bucket = "gitops-tf-backend-mysycry"
+    key    = "terraform.tfstate"
+    region = "ap-southeast-2"
+    dynamodb_table = "GitopsTerraformLocks-mysycry"
+  }
+}
